ead54aabfa73ccbda8f93f2e7bf946b56f217aa8
Emil Kallioniemi <emil@kallioniemi.se>
2023-11-28 23:58:42 +0100

    Ref WEB-13: Add console log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 export default async function Page() {
   const session = await getSession();
 
+  console.log("YAAA");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
